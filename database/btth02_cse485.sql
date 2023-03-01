8a841a1f79ce4a86a766657f866948353eae13ef
vvt911 <thuyvucuber@gmail.com>
2023-03-01 10:20:35 +0700

    update database

--- database/btth02_cse485.sql
+++ database/btth02_cse485.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 01, 2023 at 03:59 AM
+-- Generation Time: Mar 01, 2023 at 04:20 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `btth01_cse485`
+-- Database: `btth02_cse485`
 --
 
 DELIMITER $$
@@ -43,26 +43,26 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `baiviet`
+-- Table structure for table `article`
 --
 
-CREATE TABLE `baiviet` (
-  `ma_bviet` int(10) UNSIGNED NOT NULL,
-  `tieude` varchar(200) COLLATE utf8_vietnamese_ci NOT NULL,
-  `ten_bhat` varchar(100) COLLATE utf8_vietnamese_ci NOT NULL,
-  `ma_tloai` int(10) UNSIGNED NOT NULL,
-  `tomtat` text COLLATE utf8_vietnamese_ci NOT NULL,
-  `noidung` text COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  `ma_tgia` int(10) UNSIGNED NOT NULL,
-  `ngayviet` datetime NOT NULL DEFAULT current_timestamp(),
-  `hinhanh` varchar(200) COLLATE utf8_vietnamese_ci DEFAULT NULL
+CREATE TABLE `article` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(200) COLLATE utf8_vietnamese_ci NOT NULL,
+  `song_name` varchar(100) COLLATE utf8_vietnamese_ci NOT NULL,
+  `category_id` int(10) UNSIGNED NOT NULL,
+  `summary` text COLLATE utf8_vietnamese_ci NOT NULL,
+  `content` text COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `author_id` int(10) UNSIGNED NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `image` varchar(200) COLLATE utf8_vietnamese_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
--- Dumping data for table `baiviet`
+-- Dumping data for table `article`
 --
 
-INSERT INTO `baiviet` (`ma_bviet`, `tieude`, `ten_bhat`, `ma_tloai`, `tomtat`, `noidung`, `ma_tgia`, `ngayviet`, `hinhanh`) VALUES
+INSERT INTO `article` (`id`, `title`, `song_name`, `category_id`, `summary`, `content`, `author_id`, `date`, `image`) VALUES
 (2, 'Cảm ơn em đã rời xa anh', 'Vết mưa', 2, 'Cảm ơn em đã cho anh những tháng ngày hạnh phúc, cho anh biết yêu và được yêu. Em cho anh được nếm trải hương vị ngọt ngào của tình yêu nhưng cũng đầy đau khổ và nước mắt. Những tháng ngày đó có lẽ suốt cuộc đời anh không bao giờ quên', NULL, 3, '2012-02-12 00:00:00', NULL),
 (3, 'Cuộc đời có mấy ngày mai?', 'Phôi pha', 2, 'Đêm nay, trời quang mây tạnh, trong người nghe hoang vắng và tôi ngồi đây “Ôm lòng đêm, Nhìn vầng trăng mới về” mà ngậm ngùi “Nhớ chân giang hồ. Ôi phù du, từng tuổi xuân đã già”', NULL, 4, '2014-03-13 00:00:00', NULL),
 (4, 'Quê tôi!', 'Quê hương', 5, 'Quê hương là gì mà chở đầy kí ức nhỏ xinh. Có đám trẻ nô đùa bên nhau dưới gốc ổi nhà bà Năm giữa trưa nắng gắt chỉ để chờ bà đi vắng là hái trộm. Có hai anh em tôi bì bõm lội sình bắt cua đem về nhà cho mẹ nấu canh, nấu cháo… Có ba chị em tôi lục đục tự nấu ăn khi mẹ vắng nhà. Có anh tôi luôn dắt tôi đi cùng đường ngõ xóm chỉ để em được vui. Có cả những trận cãi nhau nảy lửa của ba anh em nữa…', NULL, 5, '2014-02-20 00:00:00', NULL),
@@ -77,10 +77,10 @@ INSERT INTO `baiviet` (`ma_bviet`, `tieude`, `ten_bhat`, `ma_tloai`, `tomtat`, `
 (13, 'Như một cách tạ ơn đời', 'Người thầy', 2, 'Ánh nắng cuối ngày rồi cũng sẽ tắt, dòng sông con đò rồi cũng sẽ rẽ sang một hướng khác. Nhưng việc trồng người luôn cảm thụ với chuyến đò ngang, cứ tần tảo đưa rồi lặng lẽ quay về đưa sang. Con đò năm xưa của Thầy nặng trĩu yêu thương, hy sinh thầm lặng.', NULL, 8, '2014-01-02 00:00:00', NULL);
 
 --
--- Triggers `baiviet`
+-- Triggers `article`
 --
 DELIMITER $$
-CREATE TRIGGER `tg_SuaBaiViet` AFTER UPDATE ON `baiviet` FOR EACH ROW BEGIN
+CREATE TRIGGER `tg_SuaBaiViet` AFTER UPDATE ON `article` FOR EACH ROW BEGIN
 		UPDATE theloai
     	SET SLBaiViet = (SELECT COUNT(*) FROM baiviet WHERE ma_tloai = NEW.ma_tloai)
     	WHERE ma_tloai = NEW.ma_tloai;
@@ -88,7 +88,7 @@ END
 $$
 DELIMITER ;
 DELIMITER $$
-CREATE TRIGGER `tg_ThemHBaiViet` AFTER INSERT ON `baiviet` FOR EACH ROW BEGIN
+CREATE TRIGGER `tg_ThemHBaiViet` AFTER INSERT ON `article` FOR EACH ROW BEGIN
 		UPDATE theloai
     	SET SLBaiViet = (SELECT COUNT(*) FROM baiviet WHERE ma_tloai = NEW.ma_tloai)
     	WHERE ma_tloai = NEW.ma_tloai;
@@ -96,7 +96,7 @@ END
 $$
 DELIMITER ;
 DELIMITER $$
-CREATE TRIGGER `tg_XoaBaiViet` AFTER DELETE ON `baiviet` FOR EACH ROW BEGIN
+CREATE TRIGGER `tg_XoaBaiViet` AFTER DELETE ON `article` FOR EACH ROW BEGIN
 		UPDATE theloai
         SET SLBaiViet = (SELECT COUNT(*) FROM baiviet WHERE ma_tloai = OLD.ma_tloai)
         WHERE ma_tloai = OLD.ma_tloai;
@@ -107,20 +107,20 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tacgia`
+-- Table structure for table `author`
 --
 
-CREATE TABLE `tacgia` (
-  `ma_tgia` int(10) UNSIGNED NOT NULL,
-  `ten_tgia` varchar(100) COLLATE utf8_vietnamese_ci NOT NULL,
-  `hinh_tgia` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL
+CREATE TABLE `author` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(100) COLLATE utf8_vietnamese_ci NOT NULL,
+  `image` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
--- Dumping data for table `tacgia`
+-- Dumping data for table `author`
 --
 
-INSERT INTO `tacgia` (`ma_tgia`, `ten_tgia`, `hinh_tgia`) VALUES
+INSERT INTO `author` (`id`, `name`, `image`) VALUES
 (1, 'Nhacvietplus', NULL),
 (2, 'Sưu tầm', NULL),
 (3, 'Sandy', NULL),
@@ -133,20 +133,20 @@ INSERT INTO `tacgia` (`ma_tgia`, `ten_tgia`, `hinh_tgia`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `theloai`
+-- Table structure for table `category`
 --
 
-CREATE TABLE `theloai` (
-  `ma_tloai` int(10) UNSIGNED NOT NULL,
-  `ten_tloai` varchar(50) COLLATE utf8_vietnamese_ci NOT NULL,
-  `SLBaiViet` int(11) NOT NULL DEFAULT 0
+CREATE TABLE `category` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8_vietnamese_ci NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
--- Dumping data for table `theloai`
+-- Dumping data for table `category`
 --
 
-INSERT INTO `theloai` (`ma_tloai`, `ten_tloai`, `SLBaiViet`) VALUES
+INSERT INTO `category` (`id`, `name`, `quantity`) VALUES
 (1, 'Nhạc trẻ', 1),
 (2, 'Nhạc trữ tình', 3),
 (4, 'Nhạc thiếu nhi', 0),
@@ -162,15 +162,15 @@ INSERT INTO `theloai` (`ma_tloai`, `ten_tloai`, `SLBaiViet`) VALUES
 --
 
 CREATE TABLE `user` (
-  `ten_dnhap` varchar(50) COLLATE utf8_vietnamese_ci NOT NULL,
-  `mat_khau` varchar(50) COLLATE utf8_vietnamese_ci NOT NULL
+  `username` varchar(50) COLLATE utf8_vietnamese_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8_vietnamese_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`ten_dnhap`, `mat_khau`) VALUES
+INSERT INTO `user` (`username`, `password`) VALUES
 ('tuan', '123'),
 ('thuy', '123'),
 ('phong', '123'),
@@ -184,17 +184,6 @@ INSERT INTO `user` (`ten_dnhap`, `mat_khau`) VALUES
 -- (See below for the actual view)
 --
 CREATE TABLE `vw_music` (
-`ma_bviet` int(10) unsigned
-,`tieude` varchar(200)
-,`ten_bhat` varchar(100)
-,`ma_tloai` int(10) unsigned
-,`tomtat` text
-,`noidung` text
-,`ma_tgia` int(10) unsigned
-,`ngayviet` datetime
-,`hinhanh` varchar(200)
-,`ten_tloai` varchar(50)
-,`ten_tgia` varchar(100)
 );
 
 -- --------------------------------------------------------
@@ -211,57 +200,57 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 
 --
--- Indexes for table `baiviet`
+-- Indexes for table `article`
 --
-ALTER TABLE `baiviet`
-  ADD PRIMARY KEY (`ma_bviet`),
-  ADD KEY `ma_tgia` (`ma_tgia`),
-  ADD KEY `ma_tloai` (`ma_tloai`);
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ma_tgia` (`author_id`),
+  ADD KEY `ma_tloai` (`category_id`);
 
 --
--- Indexes for table `tacgia`
+-- Indexes for table `author`
 --
-ALTER TABLE `tacgia`
-  ADD PRIMARY KEY (`ma_tgia`);
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `theloai`
+-- Indexes for table `category`
 --
-ALTER TABLE `theloai`
-  ADD PRIMARY KEY (`ma_tloai`);
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `baiviet`
+-- AUTO_INCREMENT for table `article`
 --
-ALTER TABLE `baiviet`
-  MODIFY `ma_bviet` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+ALTER TABLE `article`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT for table `tacgia`
+-- AUTO_INCREMENT for table `author`
 --
-ALTER TABLE `tacgia`
-  MODIFY `ma_tgia` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+ALTER TABLE `author`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `theloai`
+-- AUTO_INCREMENT for table `category`
 --
-ALTER TABLE `theloai`
-  MODIFY `ma_tloai` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+ALTER TABLE `category`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `baiviet`
+-- Constraints for table `article`
 --
-ALTER TABLE `baiviet`
-  ADD CONSTRAINT `baiviet_ibfk_1` FOREIGN KEY (`ma_tgia`) REFERENCES `tacgia` (`ma_tgia`),
-  ADD CONSTRAINT `baiviet_ibfk_2` FOREIGN KEY (`ma_tloai`) REFERENCES `theloai` (`ma_tloai`);
+ALTER TABLE `article`
+  ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`),
+  ADD CONSTRAINT `article_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
