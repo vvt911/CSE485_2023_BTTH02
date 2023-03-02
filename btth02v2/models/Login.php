583d8e9c769b3e693aec4219974079bac8843620
Nguyen Cong Phong <barcatrongtim2@gmail.com>
2023-03-02 11:30:04 +0700

    update file and folder

---
+++ btth02v2/models/Login.php
@@ -0,0 +1,19 @@
+<?php
+    class User{
+        private $username;
+        private $password;
+        public function __construct($username, $password)
+        {
+            $this->username = $username;
+            $this->password = $password;
+        }
+        public function getUserName(){
+            return $this->username;
+        }
+        public function getPassword(){
+            return $this->password;
+        }
+        
+    
+    }
+?>
